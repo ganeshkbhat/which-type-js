fe2875c0587b9422e3c8484e4fdebe3af48d84cb
Ganesh B <ganeshsurfs@gmail.com>
2024-03-05 16:06:01 +0530

    adding minor changes to validators. removing functions that are not comprehensive. adding compatibility for browsers

--- demos/demos.types.js
+++ demos/demos.types.js
@@ -1,56 +1,56 @@
-// /**
-//  * 
-//  * Package: 
-//  * Author: Ganesh B
-//  * Description: 
-//  * Install: npm i  --save
-//  * Github: https://github.com/ganeshkbhat/
-//  * npmjs Link: https://www.npmjs.com/package/
-//  * File: 
-//  * File Description: 
-//  * 
-// */
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: 
+ * File Description: 
+ * 
+*/
 
-// /* eslint no-console: 0 */
+/* eslint no-console: 0 */
 
-// 'use strict';
+'use strict';
 
-// const { TypeTester } = require('../index');
-// const { TagTester,
-//   isBoolean,
-//   isObject,
-//   isNull,
-//   isUndefined,
-//   isElement,
-//   isString,
-//   isNumber,
-//   isDate,
-//   isRegExp,
-//   isError,
-//   isSymbol,
-//   isFunction,
-//   isDataView,
-//   isValidDataView,
-//   isTagObject,
-//   isBufferLike } = require('../index');
+const { TypeTester } = require('../index');
+const { TagTester,
+  isBoolean,
+  isObject,
+  isNull,
+  isUndefined,
+  isElement,
+  isString,
+  isNumber,
+  isDate,
+  isRegExp,
+  isError,
+  isSymbol,
+  isFunction,
+  isDataView,
+  isValidDataView,
+  isTagObject,
+  isBufferLike } = require('../index');
 
 
-// console.log("isString: ", true, isString("tester"))
-// console.log("isNumber : ", true, isNumber(1))
-// console.log("isBoolean : ", true, isBoolean(true))
-// console.log("isDate : ", false, isDate(Date.now()))
-// console.log("isDate : ", true, isDate(new Date(Date.now())))
-// var isArray = TagTester("Array");
-// console.log("isArray : ", true, isArray([1, 2, 3]))
-// console.log("isRegExp : ", true, isRegExp(new RegExp(/^d/g)))
-// console.log("isRegExp : ", true, isRegExp(/^d/))
-// console.log("isError : ", false, isError(Error))
-// console.log("isError : ", true, isError(new Error("Tester")))
-// console.log("isSymbol : ", true, isSymbol(Symbol("Tester")))
-// let isArrayBuffer = TypeTester("ArrayBuffer");
-// console.log("isArrayBuffer : ", false, isArrayBuffer([]))
-// console.log("isArrayBuffer : ", false, isArrayBuffer(Buffer.from([])))
-// console.log("isArrayBuffer : ", true, isArrayBuffer(new ArrayBuffer([])))
-// console.log("isFunction : ", true, isFunction(() => { }))
-// console.log("isDataView : ", false, isDataView([]))
-// console.log("isTagObject : ", true, isTagObject({ "sc": 10 }))
+console.log("isString: ", true, isString("tester"))
+console.log("isNumber : ", true, isNumber(1))
+console.log("isBoolean : ", true, isBoolean(true))
+console.log("isDate : ", false, isDate(Date.now()))
+console.log("isDate : ", true, isDate(new Date(Date.now())))
+var isArray = TagTester("Array");
+console.log("isArray : ", true, isArray([1, 2, 3]))
+console.log("isRegExp : ", true, isRegExp(new RegExp(/^d/g)))
+console.log("isRegExp : ", true, isRegExp(/^d/))
+console.log("isError : ", false, isError(Error))
+console.log("isError : ", true, isError(new Error("Tester")))
+console.log("isSymbol : ", true, isSymbol(Symbol("Tester")))
+let isArrayBuffer = TypeTester("ArrayBuffer");
+console.log("isArrayBuffer : ", false, isArrayBuffer([]))
+console.log("isArrayBuffer : ", false, isArrayBuffer(Buffer.from([])))
+console.log("isArrayBuffer : ", true, isArrayBuffer(new ArrayBuffer([])))
+console.log("isFunction : ", true, isFunction(() => { }))
+console.log("isDataView : ", false, isDataView([]))
+console.log("isTagObject : ", true, isTagObject({ "sc": 10 }))
